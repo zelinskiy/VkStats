390c3d4bd5329a669f09ba7cfd4b58bf1eeb2a26
zelinskiy <zelinskiy1917@gmail.com>
2017-04-05 15:00:18 +0300

    Basic SQLite connection

--- VkStats/Program.cs
+++ VkStats/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SQLite.CodeFirst;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -8,66 +11,46 @@ using System.Threading.Tasks;
 using VkNet;
 using VkNet.Enums.Filters;
 using VkNet.Model.RequestParams;
-
-
+using VkStats.Models;
 
 namespace VkStats
 {
-    class Program
+    public class Person
     {
+        public Int64 Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class MyContext : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<MyContext>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+
+    class Program
+    {      
+        
         static void Main(string[] args)
         {
-            // There must be file secrets.txt with
-            // LOGIN
-            // PASS
-            // APPID
-            var storedSecrets = File.ReadLines("..\\..\\secrets.txt").ToArray();
-            var login = storedSecrets[0];
-            var pass = storedSecrets[1];
-            var applicationId = ulong.Parse(storedSecrets[2]);
-
-            const int groupId = 44898240;
-            const string groupName = "kanonrk";
-
-            var vk = new VkApi();
-            vk.Authorize(new ApiAuthParams
+            Console.WriteLine("BEGIN");
+            using (var db = new MyContext())
             {
-                ApplicationId = applicationId,
-                Login = login,
-                Password = pass,
-                Settings = Settings.All
-            });
-            
-            var members = vk.Groups.GetMembers(new GroupsGetMembersParams
-            {
-                GroupId = groupName,
-                Count = 50,
-                Fields = UsersFields.All
-            });
-            
-            Console.WriteLine("Loaded {0} ids", members.Count);
-            foreach(var m in members.Take(5))
-            {
-                Console.WriteLine("========================");
-                Console.WriteLine("User {0} {1} top 20:", m.FirstName, m.LastName);
-                Console.WriteLine("========================");
-                var groups = vk.Groups.Get(new GroupsGetParams
+                var person = new Person() { Name = "Martha" };
+                db.Persons.Add(person);
+                db.SaveChanges();
+                foreach(var p in db.Persons.ToArray())
                 {
-                    UserId = m.Id,
-                    Count = 20,
-                    Extended = true,
-                    Filter = GroupsFilters.Publics,
-                    Fields = GroupsFields.All
-                });
-                foreach (var g in groups)
-                {
-                    Console.WriteLine(g.Name);
+                    Console.WriteLine(p.Name);
                 }
-                
             }
-
-            Console.WriteLine("END OF PROGRAM");
+            
+            Console.WriteLine("END");
             Console.ReadKey();
         }
+        
     }
 }
